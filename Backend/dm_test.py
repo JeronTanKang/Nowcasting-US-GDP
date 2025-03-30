0147dae5caa60d7f2574f42fe4db393448769073
Jeron Tan Kang <124889093+JeronTanKang@users.noreply.github.com>
2025-03-30 16:43:32 +0800

    Update and rename DM_test.py to dm_test.py

---
+++ Backend/dm_test.py
@@ -0,0 +1,10 @@
+import numpy as np
+import pandas as pd 
+import os
+import sys
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'Backend')))
+
+#from diebold_mariano_test import diebold_mariano_test 
+
+def dm_test("input from forecast_evaluation"):
+    pass
